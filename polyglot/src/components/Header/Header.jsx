a17e31a29cd0cbce9c933ba6ee0256671cbafe5c
Undery33 <xodml1122@naver.com>
2024-09-06 18:25:51 +0900

    add Design

--- polyglot/src/components/Header/Header.jsx
+++ polyglot/src/components/Header/Header.jsx
@@ -6,14 +6,27 @@ const Header = () => {
     return (
         <header className="site-header">
             <nav className="main-nav">
-            <Link to="/" className="logo"><img src="/images/logo.png" alt="Main logo" className="mainlogo"/></Link>
+                <Link to="/" className="logo">
+                <img src="/images/logo.png" alt="Main logo" className="mainlogo" />
+                </Link>
+
+                <div className="nav-center">
                 <Link to="Polyglot" className="info-link">POLYGLOT</Link>
                 <Link to="How2use" className="info-link">How To Use</Link>
                 <Link to="Issue" className="info-link">Issue</Link>
+                </div>
+
+                <div className="nav-right">
                 <a href="https://discord.com/">Discord</a>
-                <button className="lang-button">Language</button>
+                <button className="lang-button">
+                    <img src="/images/lang.png" className="langicon" />Language
+                </button>
+                </div>
             </nav>
         </header>
+
+
+
     )
 }
 
--- polyglot/src/components/Section/Section.jsx
+++ polyglot/src/components/Section/Section.jsx
@@ -8,6 +8,11 @@ const Section = () => {
                 <h1>실시간 번역, 자연스러운 대화</h1>
                 채팅으로 번역하던 시절은 안녕, 대화로 실시간 번역을 디스코드에서도 체험해보세요. <br/>
                 
+                <div>
+                    
+
+                </div>
+
                 <button>
                 <div className="discord">
                     <img src="./images/discord.svg"></img>
@@ -16,14 +21,16 @@ const Section = () => {
                 </button>
             </div>
 
-
-            <br/><br/>
-            <h1>TTS로 듣는 깔끔한 음성</h1>
-            어떻게 실시간 번역이 가능하냐고요? 그야 TTS가 대신 답해주거든요
-            <br/><br/>
-            <h1>영어부터 시작해서 쭈욱 끝까지</h1>
-            많이 사용하는 영어부터 한국어, 일본어, 더 나아가 불어까지
-            <br/><br/>
+            <div className="second-text">
+                <h1>TTS로 듣는 깔끔한 음성</h1>
+                어떻게 실시간 번역이 가능하냐고요? 그야 TTS가 대신 답해주거든요
+            </div>
+            
+            <div className="third-text">
+                <h1>영어부터 시작해서 쭈욱 끝까지</h1>
+                많이 사용하는 영어부터 한국어, 일본어, 더 나아가 불어까지
+            </div>
+            
         </>
     )
 }
