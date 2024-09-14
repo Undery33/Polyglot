48e0d790a5a5165d95f8f86e153e9885cf364d48
Undery33 <htu123132@gmail.com>
2024-09-15 02:38:39 +0900

    commit test

--- poly-bot/commands/utility/server.js
+++ poly-bot/commands/utility/server.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Provides information about the server.'),
+		.setName('서버정보')
+		.setDescription('서버 정보를 보여드려요'),
 	async execute(interaction) {
 		
 		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
